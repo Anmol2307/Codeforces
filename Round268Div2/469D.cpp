aee2822c7fdbcdc0a954037e13a4369fed956e3c
Anmol <garganmol1993@gmail.com>
2014-09-23 05:05:38 +0530

    468 D solved

--- Round268Div2/469D.cpp
+++ Round268Div2/469D.cpp
@@ -11,73 +11,56 @@ inline void inp(int &n ) {//fast input function
     n=n*sign;
 }
 
+map<int, int> mymap;
 
 int main () {
   int n, a, b;
   inp(n); inp(a); inp(b);
-  vector <int> arr;
-  vector <int> brr;
-  map <int, int> m;
+
+  int arr[n];
+  int ans[n];
   for (int i = 0; i < n; i++) {
-    int x; inp(x);
-    arr.push_back(x);
-    brr.push_back(x);
-    m.put(pair<int,int>(x,-1));
+    inp(arr[i]);
+    mymap[arr[i]] = i;
   }
-
-  sort(brr.begin(),brr.end());
-  int set[n];
-  memset(set,-1,sizeof(set));
-  
-  // map<int,int>::iterator it;
-
-  // for (it = m.begin(); it != m.end(); it++) {
-  //   if (it->second == -1) {
-  //     map<int,int>::iterator tmp = m.find(a-it->first);
-  //     if ((tmp != m.end()) && tmp->second == -1) {
-  //       it->second = 0;
-  //       tmp->second = 0;
-  //       continue;
-  //     }
-  //     tmp = m.find(b-it->first);
-  //     if ((tmp != m.end()) && tmp->second == -1) {
-  //       it->second = 1;
-  //       tmp->second = 1;
-  //       continue;
-  //     }
-  //   }
-  // }
 
   bool flag = false;
-  vector<int>::iterator it = arr.begin();
-  for (; it != arr.end(); it = arr.begin()) {
-    
-    if (set[i] == -1) {
-      if (binary_search(brr.begin(),brr.end(),a - arr[i])) {
-        set[i] = 0;
-        vector<int>::iterator it = lower_bound(brr.begin(),brr.end(),a-arr[i]);
-        brr.erase(it);
-        set[it-brr.begin()] = 0;
-      }
-      else if (binary_search(brr.begin(),brr.end(),b - arr[i])) {
-        set[i] = 1;
-        vector<int>::iterator it = lower_bound(brr.begin(),brr.end(),b-arr[i]);
-        brr.erase(it);
-        set[it-brr.begin()] = 1;
-      }
-      else {
-        flag = true;
-        printf("NO\n");
-        break;
-      }
-    }
+  if (a > b) {
+    swap(a,b);
+    flag = true;
   }
 
-  if (!flag) {
-    printf("YES\n");
-    for (int i = 0; i < n; i++) {
-      printf("%d ",set[i]);
+  map<int,int>::iterator it;
+  while (!mymap.empty()) {
+    it = mymap.begin();
+    // it--;
+    int first = it->first;
+    int index = it->second;
+
+    if (mymap.find(b-first) != mymap.end()) {
+      ans[index] = 1;
+      ans[mymap[b-first]] = 1;
+      mymap.erase(first);
+      mymap.erase(b-first);
+      continue;
     }
+    if (mymap.find(a-first) != mymap.end()) {
+      ans[index] = 0;
+      ans[mymap[a-first]] = 0;
+      mymap.erase(first);
+      mymap.erase(a-first);
+      continue;
+    }
+    printf("NO\n");
+    return 0;
+  }
+  printf("YES\n");
+  if(flag) {
+    for(int i=0; i < n; i++) printf("%d ",1-ans[i]);
     printf("\n");
+  }
+  else {
+    for(int i=0; i < n; i++) printf("%d ",ans[i]);
+      printf("\n");
   }
 }
